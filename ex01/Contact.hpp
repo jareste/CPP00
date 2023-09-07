8857a17e783834d3fd0dc865563f552169eb16e7
Joan Areste Gonzalez <jareste2000@gmail.com>
2023-09-07 17:16:10 +0200

    oktorok wants to delete it

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,10 +6,28 @@
 /*   By: jareste- <jareste-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 20:21:15 by jareste-          #+#    #+#             */
-/*   Updated: 2023/08/29 22:54:10 by jareste-         ###   ########.fr       */
+/*   Updated: 2023/09/07 17:14:11 by jareste-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+#ifndef CONTACT_H
+ #define CONTACT_H
 
+class Contact
+{
+	private:
+		std::string _first_name;
+		std::string _last_name;
+		std::string _nickname;
+		std::string _phone_number;
+		std::string _darkest_secret;
+
+	public:
+}
+
+
+
+
+#endif
